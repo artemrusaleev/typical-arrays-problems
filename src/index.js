8c09c05f4f981bd5349ae629571782ce625fc3a9
Mariarty <artyomrusaleev@gmail.com>
2021-03-09 14:47:23 +0300

    feat: Done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array){
+    return 0
+  }
+  if(array.length === 0 ){
+    return array
+  }
+  let min = array[0];
+  array.forEach(el=>{
+    if(el<min){
+      min = el;
+    }
+  })
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array){
+    return 0
+  }
+  if(array.length === 0 ){
+    return array
+  }
+  let max = array[0];
+  array.forEach(el=>{
+    if(el>max){
+      max = el;
+    }
+  })
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array){
+    return 0
+  }
+  if(array.length === 0 ){
+    return array
+  }
+  let avg = 0
+  array.forEach(el => {
+    return avg += el
+  });
+  return avg/array.length
 }
